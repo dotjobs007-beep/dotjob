8a3ec98aab54ca5bbc265367c95a2fb2e0050d20
Festus <festusishioma123@gmail.com>
2025-09-26 12:59:05 +0100

    added validation

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -7,6 +7,7 @@ const jobController = new JobController();
 
 router.post("/post-job", validateBody(jobSchema), jobController.postJob);
 router.get("/fetch-jobs", jobController.getAllJob);
+
 router.get("/fetch-job/:jobId", jobController.getJob);
 router.get("/fetch-job-by-user", jobController.getUserJob);
 router.patch(
